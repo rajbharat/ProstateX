2679fe6c4c8d412d9f50b87c79de509c80f1bcfd
Alexander Hamilton <alexanderhamilton@outlook.com>
2018-11-15 22:02:33 -0500

    Updated conversion and re-sampling scripts

--- src/02-preprocessing/02_convert2nifti.py
+++ src/02-preprocessing/02_convert2nifti.py
@@ -1,11 +1,13 @@
 """
 Author: Alex Hamilton - https://github.com/alexhamiltonRN
 Created: 2018-11-14
-Description: A script to convert raw data (DICOM images) in ProstateX challenge to nifti format
+Description: A script to convert raw data (DICOM and mhd images) in ProstateX challenge 
+to nifti format
 """
 
 import pandas as pd
 import dicom2nifti
+import SimpleITK as sitk
 import os
 from pathlib import Path
 
@@ -14,10 +16,6 @@ def generate_paths_to_dicom(is_training_data):
     This function generates a dictionary containing the patient id as the 
     primary key and assigns a second dictionary containing the paths to the 
     t2, adc, and bval dicom folders for conversion to nifti later
-
-    Directory structure for raw data:
-    ProstateX/raw/train/dicom_train_204
-    ProstateX/raw/test/dicom_train_140
     """
     
     path_to_data = Path()
@@ -49,9 +47,7 @@ def generate_paths_to_dicom(is_training_data):
             
             paths_to_dicom[patient_path] = {'t2':t2_path, 'adc': adc_path, 'bval':bval_path} 
     
-    #TODO: write a dataframe containing the patient id | t2_path | adc_path | bval_path
-    #dataframe = pd.DataFrame(paths_to_dicom)
-    print('Done generating paths to dicom files for conversion...')
+    print('Done generating paths to dicom files for conversion...\n')
     return paths_to_dicom
 
 def convert_dicom2nifti(paths_to_dicom, is_training_data):
@@ -59,12 +55,8 @@ def convert_dicom2nifti(paths_to_dicom, is_training_data):
     This function does the actual conversion of dicom files to nifti by 
     supplying the dicom2nifti convert_directory method with a path to the 
     original dicom files and to a new directory.
-
-    * Note: this failed on approximately 40 patients in the previous dataset. 
-    I am going to try a try:except pattern to continue on in the loop. There
-    is a command line tool to remedy this problem - but it is challenging to
-    install and configure.
     """
+    print('Generating nifti files from dicom...\n')
     
     path_to_nifti = Path()
 
@@ -76,13 +68,11 @@ def convert_dicom2nifti(paths_to_dicom, is_training_data):
     counter = 1
 
     for patient_id, file_structure in paths_to_dicom.items():
-        
-        # DICOM Paths
+         
         dicom_t2_path = file_structure['t2']
         dicom_adc_path = file_structure['adc']
         dicom_bval_path = file_structure['bval']
 
-        # NIFTI Paths
         nifti_t2_path = path_to_nifti.joinpath(str(patient_id) + '/t2')
         nifti_adc_path = path_to_nifti.joinpath(str(patient_id) + '/adc')
         nifti_bval_path = path_to_nifti.joinpath(str(patient_id) + '/bval')
@@ -91,7 +81,7 @@ def convert_dicom2nifti(paths_to_dicom, is_training_data):
             dicom2nifti.convert_directory(str(dicom_t2_path), str(nifti_t2_path))
             dicom2nifti.convert_directory(str(dicom_adc_path), str(nifti_adc_path))
             dicom2nifti.convert_directory(str(dicom_bval_path), str(nifti_bval_path))
-            print('Successful conversion of: ' + patient_id, counter)
+            print('Successful dicom to nifti conversion of: ' + patient_id, counter)
             counter = counter + 1
 
         except:
@@ -106,6 +96,49 @@ def convert_dicom2nifti(paths_to_dicom, is_training_data):
                 f.close()
                 print('Problem with:', patient_id)
             continue
+   
+def convert_mhd2nifti(is_training_data):
+    """
+    This function converts mhd (ktrans) files to nifti. It uses SimpleITK ImageFileReader and 
+    ImageFileWriter functions to execute the conversion.
+    """
+    
+    print('\n')
+    print('Generating nifti files from mhd...\n')
+
+    if is_training_data:
+        path_to_nifti = Path('/home/alex/Documents/DataProjects/Data/MBI/ProstateX/generated/train/nifti/')
+        path_to_ktrans_data = Path('/home/alex/Documents/DataProjects/Data/MBI/ProstateX/raw/train/ktrans_train_204')
+    else:
+        path_to_nifti = Path('/home/alex/Documents/DataProjects/Data/MBI/ProstateX/generated/test/nifti')
+        path_to_ktrans_data = Path('/home/alex/Documents/DataProjects/Data/MBI/ProstateX/raw/test/ktrans_test_140')
+
+    counter = 1
+    patient_folders = [x for x in path_to_ktrans_data.iterdir() if x.is_dir()]
+
+    for patient in patient_folders:
+        patient_id = patient.stem
+
+        mhd_files = []
+        
+        for item in patient.rglob('*.mhd'):
+            mhd_files.append(item)
+
+        full_path_to_mhdfile = mhd_files[0]
+        full_path_to_ktrans_folder = path_to_nifti / patient_id / 'ktrans'
+        filename = patient_id + '-ktrans.nii.gz' 
+        new_file_path = full_path_to_ktrans_folder / filename
+        
+        reader = sitk.ImageFileReader()
+        reader.SetFileName(str(full_path_to_mhdfile))
+        image = reader.Execute()
+
+        writer = sitk.ImageFileWriter()
+        writer.SetFileName(str(new_file_path))
+        writer.Execute(image)    
+
+        print('Successful mhd to nifti conversion of: ' + patient_id, counter)
+        counter = counter + 1
 
 def main():
     is_training_data = False
@@ -116,5 +149,6 @@ def main():
     
     paths_to_dicom = generate_paths_to_dicom(is_training_data)
     convert_dicom2nifti(paths_to_dicom, is_training_data)
+    convert_mhd2nifti(is_training_data)
     
 main()
