f352c77a3ea30da019e5f6736851f19a923f9c2f
Alexander Hamilton <alexanderhamilton@outlook.com>
2018-11-19 16:15:43 -0500

    Generated CNN models

--- src/02-modeling/ktrans_cnn.py
+++ src/02-modeling/ktrans_cnn.py
@@ -7,20 +7,23 @@ Description: CNN training using ktrans images from ProstateX challenge.
 import keras
 from keras import layers
 from keras import models
+from keras import regularizers
+from keras.utils import plot_model
+
+import training_plots
 
 import numpy as np
 from pathlib import Path
 
-# Loading the data
+# LOADING THE DATA
 ktrans_samples = np.load('/home/alexander/Documents/DataProjects/Data/MBI/ProstateX/generated/train/numpy/ktrans/X_train.npy')
 ktrans_labels = np.load('/home/alexander/Documents/DataProjects/Data/MBI/ProstateX/generated/train/numpy/ktrans/Y_train.npy')
 
-# Convert image samples to float32
+# CONVERT IMAGE SAMPLES TO FLOAT32 (REDUCE PRECISION FROM FLOAT64)
 ktrans_samples_flt32 = np.array(ktrans_samples, dtype=np.float32, copy = True)
 
-# Reshape image samples to include single channel
-x_train = ktrans_samples_flt32.reshape((617,8,8,1))
-
+# RESHAPE IMAGE SAMPLES TO INCLUDE A SINGLE CHANNEL
+x_train = ktrans_samples_flt32.reshape((620,8,8,1))
 y_train = ktrans_labels
 
 # MODEL SPECIFICATION
@@ -28,22 +31,32 @@ model = models.Sequential()
 
 model.add(layers.Conv2D(32, kernel_size=(3,3), padding = 'same', activation='relu', input_shape=(8,8,1))) 
 model.add(layers.Conv2D(32, (3,3), activation='relu'))
-model.add(layers.Dropout(0.25))
-
-model.add(layers.Conv2D(64, (3,3), padding = 'same', activation='relu'))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.Dropout(0.25))
+model.add(layers.Dropout(0.5))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Dropout(0.5))
 
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+
 model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Dense(128, kernel_regularizer = regularizers.l1_l2(l1=0.0001, l2=0.0001), activation='relu'))
 model.add(layers.Dropout(0.5))
 
 model.add(layers.Dense(1, activation='sigmoid'))
 model.summary()
 
 # COMPILATION
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+opt = keras.optimizers.Adadelta()
+model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 
 # FIT
-model.fit(x_train, y_train, epochs=20, validation_split=0.25, batch_size=32, shuffle=True)
+history = model.fit(x_train, y_train, epochs=100, validation_split=0.25, class_weight={0:1, 1:2}, batch_size=80, shuffle=True)
+
+# PLOT ACCURACY/VALIDATION CURVES
+plot_model(model, to_file='ktrans_model.png', show_shapes = True)
+training_plots.plot_metrics(history, 'KTRANS')
--- src/02-modeling/t2_cnn.py
+++ src/02-modeling/t2_cnn.py
@@ -7,20 +7,23 @@ Description: CNN training using t2 images from ProstateX challenge.
 import keras
 from keras import layers
 from keras import models
+from keras import regularizers
+from keras.utils import plot_model
+
+import training_plots
 
 import numpy as np
 from pathlib import Path
 
-# Loading the data
+# LOADING THE DATA
 t2_samples = np.load('/home/alexander/Documents/DataProjects/Data/MBI/ProstateX/generated/train/numpy/t2/X_train.npy')
 t2_labels = np.load('/home/alexander/Documents/DataProjects/Data/MBI/ProstateX/generated/train/numpy/t2/Y_train.npy')
 
-# Convert image samples to float32
+# CONVERT IMAGE SAMPLES TO FLOAT32 (REDUCE PRECISION FROM FLOAT64)
 t2_samples_flt32 = np.array(t2_samples, dtype=np.float32, copy = True)
 
-# Reshape image samples to include single channel
+# RESHAPE IMAGE SAMPLES TO INCLUDE A SINGLE CHANNEL
 x_train = t2_samples_flt32.reshape((620,32,32,1))
-
 y_train = t2_labels
 
 # MODEL SPECIFICATION
@@ -28,26 +31,38 @@ model = models.Sequential()
 
 model.add(layers.Conv2D(32, kernel_size=(3,3), padding = 'same', activation='relu', input_shape=(32,32,1))) 
 model.add(layers.Conv2D(32, (3,3), activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Dropout(0.25))
+model.add(layers.Dropout(0.5))
 
 model.add(layers.Conv2D(64, (3,3), padding = 'same', activation='relu'))
 model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Dropout(0.25))
+model.add(layers.Dropout(0.5))
 
 model.add(layers.Flatten())
-model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Dense(512,activation='relu'))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Dense(128, kernel_regularizer = regularizers.l1_l2(l1=0.0001, l2=0.0001), activation='relu'))
 model.add(layers.Dropout(0.5))
 
 model.add(layers.Dense(1, activation='sigmoid'))
 model.summary()
 
 # COMPILATION
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+opt = keras.optimizers.Adadelta()
+model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 
 # FIT
-model.fit(x_train, y_train, epochs=10, validation_split=0.25, batch_size=32, shuffle=True)
+history = model.fit(x_train, y_train, epochs=100, validation_split=0.25, class_weight={0:1,1:2}, batch_size=80, shuffle=True)
 
-#TODO: Implement csv logger - see KERAS documentation
+# PLOT ACCURACY/VALIDATION CURVES
+plot_model(model, to_file='t2_model.png', show_shapes = True)
+training_plots.plot_metrics(history, 'T2')
